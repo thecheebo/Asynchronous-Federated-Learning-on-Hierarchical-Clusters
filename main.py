5ad56a3cf2df4369900c0bb131326e4706d3cade
Yijun Wang <yijun.wang@verizonmedia.com>
2021-03-20 12:58:22 -0500

    leader sequential final

--- main.py
+++ main.py
@@ -19,22 +19,102 @@ from data_utils import split_data, CustomSubset
 torch.manual_seed(42)
 np.random.seed(42)
 
+# Global vars
+testloader = None
+test_data = None
+client_datas = None
+
+server = None
+clients = []
+leaders = []
+
+cfl_stats = ExperimentLogger()
+acc_server = 0.0
 
 def main(N_CLIENTS, N_LEADERS, TRAIN_ROUNDS, SELECT_CLIENT_FRAC):
+    global server, clients, leaders
+
+    prepare_data(N_CLIENTS)
+
+    ### Create components ###
+    print("--> Creating components...")
+    server = Server(CF10Net, test_data, testloader)
+    clients = [Client(CF10Net, lambda x : torch.optim.SGD(x, lr=0.001, momentum=0.9), dat, idnum=i) for i, dat in enumerate(client_datas)]
     
+    if N_LEADERS > 0:
+        leaders = [Leader(CF10Net, test_data, i) for i in range(N_LEADERS)]
+        for i, client in enumerate(clients):
+            client.leader_id=int(i/int((N_CLIENTS/N_LEADERS)))
+            leader_id=int(client.id / int((N_CLIENTS/N_LEADERS)))
+            leaders[leader_id].client_list.append(client)
+
+    ### Train ###
+    print("--> Begin training...")
+    for c_round in range(1, TRAIN_ROUNDS + 1):
+    
+        print("Round: ", c_round)
+        for client in clients:
+            client.synchronize_with_server(server)
+
+        if N_LEADERS > 0:
+            train_with_leader()
+        else: 
+            train_baseline()
+
+        eval(c_round)
+
+    print("--> Accurary Result: ", acc_server)
+
+
+def eval(c_round):
+    global acc_server
+    acc_server = [server.evaluate()]
+    cfl_stats.log({"acc_server" : acc_server, "rounds" : c_round})
+    display_train_stats(cfl_stats, TRAIN_ROUNDS)
+
+
+def train_with_leader():
+    for leader in leaders:
+        print("   leader ", leader.id)
+        participating_clients = leader.select_clients(leader.client_list, frac = SELECT_CLIENT_FRAC)
+        for client in participating_clients:
+            print("      Client ", client.id)
+            train_stats = client.compute_weight_update(epochs=1)
+            client.send_dW_to_leader(leader)
+            client.reset()
+        leader.compute_dw_avg()
+
+    server.aggregate_avg_dws(leaders)
+
+
+def train_baseline():
+    participating_clients = server.select_clients(clients, frac = SELECT_CLIENT_FRAC)
+    for client in participating_clients:
+        print("      Client ", client.id)
+        train_stats = client.compute_weight_update(epochs=1)
+        client.reset()
+
+    server.aggregate_weight_updates(participating_clients)
+
+
+def prepare_data(N_CLIENTS):
+    print("--> Preparing and splitting data...")
+
+    global testset, testloader, client_datas
+
     ### Prepare data ###
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    
+
     trainset = torchvision.datasets.CIFAR10(root='~/data', train=True, download=False, transform=transform)
-    
+
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=0)
-    
+
     testset = torchvision.datasets.CIFAR10(root='~/data', train=False, download=False, transform=transform)
-    
+
     testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=0)
-    
+
     classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-    
+
     ### Split data ###
     DIRICHLET_ALPHA = 10.0
     train_idcs, test_idcs = np.random.permutation(len(trainset)), np.random.permutation(len(testset))
@@ -50,62 +130,6 @@ def main(N_CLIENTS, N_LEADERS, TRAIN_ROUNDS, SELECT_CLIENT_FRAC):
     client_datas = [CustomSubset(trainset, idcs) for idcs in client_idcs]
     test_data = CustomSubset(testset, test_idcs, transforms.Compose([transforms.ToTensor()]))
     
-    ### Create components ###
-    server = Server(CF10Net, test_data, testloader)
-    
-    if N_LEADERS > 0:
-        clients = [Client(CF10Net, lambda x : torch.optim.SGD(x, lr=0.001, momentum=0.9), dat, idnum=i, leader_id=int(i/int((N_CLIENTS/N_LEADERS))))
-               for i, dat in enumerate(client_datas)]
-        leaders = [Leader(CF10Net, test_data, i) for i in range(N_LEADERS)]
-        for client in clients:
-            leader_id=int(client.id / int((N_CLIENTS/N_LEADERS)))
-            leaders[leader_id].client_list.append(client)
-    else:
-        clients = [Client(CF10Net, lambda x : torch.optim.SGD(x, lr=0.001, momentum=0.9), dat, idnum=i)
-               for i, dat in enumerate(client_datas)]
-
-
-    ### Train ###
-    cfl_stats = ExperimentLogger()
-    
-    for c_round in range(1, TRAIN_ROUNDS + 1):
-    
-        print("Round: ", c_round)
-        for client in clients:
-            client.synchronize_with_server(server)
-
-        # With leader
-        if N_LEADERS > 0:
-            for leader in leaders:
-                print("   leader ", leader.id)
-                participating_clients = leader.select_clients(leader.client_list, frac = SELECT_CLIENT_FRAC)
-                for client in participating_clients:
-                    print("      Client ", client.id)
-                    train_stats = client.compute_weight_update(epochs=1)
-                    client.send_dW_to_leader(leader)
-                    client.reset()
-
-                leader.compute_dw_avg()
-
-            server.aggregate_avg_dws(leaders)
-        # Baseline
-        else: 
-            participating_clients = server.select_clients(clients, frac = SELECT_CLIENT_FRAC)
-            for client in participating_clients:
-                print("      Client ", client.id)
-                train_stats = client.compute_weight_update(epochs=1)
-                client.reset()
-    
-            server.aggregate_weight_updates(participating_clients)
-    
-        acc_server = [server.evaluate()]
-    
-        cfl_stats.log({"acc_server" : acc_server, "rounds" : c_round})
-    
-        display_train_stats(cfl_stats, TRAIN_ROUNDS)
-
-    print("Accurary Result: ", acc_server)
-
 
 if __name__ == "__main__":
     try:
